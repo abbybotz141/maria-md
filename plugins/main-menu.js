8d7a4d7df9410a9263d0c3081ccacaecef193828
abbybotz141 <abbyteam21@gmail.com>
2025-05-09 20:39:46 +0100

    GOD FIRST♥️♥️

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -3,7 +3,6 @@ const { cmd, commands } = require('../command');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
-const { h, m, s } = require('../lib/functions');
 
 // Path configuration
 const mediaPath = {
