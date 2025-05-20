fce568134458ede2618a2b1a46c0695e94da91dc
abbybotz141 <abbyteam21@gmail.com>
2025-05-20 09:18:39 +0100

    GOD FIRST♥️♥️

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms, downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = config.PREFIX
+const prefix = '#'
 
 const ownerNumber = ['2347032411938']
 
